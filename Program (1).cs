af341b5d28fcac4ed9c1b0e5433865e5dea6441f
haneenahmed2485 <121361101+haneenahmed2485@users.noreply.github.com>
2022-12-26 21:46:13 +0200

    Add files via upload

---
+++ Program (1).cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class program
+    {
+        static bool isprfecet(int x)
+        {
+            int sum = 0;
+            for (int i = 1; i < x; i++)
+            {
+                if (x % i == 0) sum = sum + i;
+            }
+            if (sum == x) return true;
+            return false;
+        }
+        static void Main(string args)
+        {
+            int y, z;
+            Console.WriteLine("enter the fiest number");
+            y = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the second number");
+            z = int.Parse(Console.ReadLine());
+            for (int i = y; i < z; i++)
+            {
+                if (isprfecet(i)) Console.WriteLine("{0},");
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
